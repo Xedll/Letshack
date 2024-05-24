5e0e4c50771de3ec9f98aa3be6934f08e8111163
Xedll <97981835+Xedll@users.noreply.github.com>
2024-05-25 00:35:56 +0300

    Setup

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { Provider } from "react-redux"
+import store from "./redux/store.js"
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/hello",
+		element: <p>hello</p>,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
 )
