a9286a5cb060a8104f39dc921cda4e8a9420bc8e
vekusxd <batmanovemil@gmail.com>
2024-05-27 00:29:15 +0300

    Delete vite-project directory

--- vite-project/src/Components/SmallText.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const SmallText = ({ text }) => {
-	return <p className='text-[#AAABAD]'>{text}</p>
-}
--- vite-project/src/Components/Tag.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Tag = ({ text }) => {
-	return (
-		<div className='cursor-pinter select-none bg-[#FFF] px-2 py-1  border-rounded rounded-xl border-2 border-pink border-solid text-pink w-fit h-fit font-semibold text-xs'>
-			{text}
-		</div>
-	)
-}
